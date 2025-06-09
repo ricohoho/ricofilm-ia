81836d5a787ed20b633892e7541d28e9cefa175c
ricohoho <ricohoho@gmail.com>
2025-06-09 19:11:31 +0200

    optimisation app.run .

--- RicoSrviceIA.py
+++ RicoSrviceIA.py
@@ -418,4 +418,4 @@ def remove_trailing_parenthesis(text):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000,debug=True, use_reloader=False)
